ab7d49d847f948f5fe973f7090af0d2168d21735
Tyler Neely <t@jujit.su>
2020-07-29 11:18:09 +0200

    Fix sled transactions and resume their usage

--- src/versioned_storage.rs
+++ src/versioned_storage.rs
@@ -31,46 +31,45 @@ impl VersionedStorage {
         // we use a sled transaction to push all concurrency concerns into the db,
         // so we can be as massively concurrent as we desire in the rest of the
         // server code
-        let ret = {
-            let raw_value_opt = self.db.get(&key).expect("db io issue");
+        self.db
+            .transaction::<_, _, ()>(|db| {
+                let raw_value_opt = db.get(&key).expect("db io issue");
 
-            let (current_ballot, current_value) =
-                if let Some(ref raw_value) = raw_value_opt {
-                    let current_ballot =
-                        u64::from_le_bytes(raw_value[..8].try_into().unwrap());
+                let (current_ballot, current_value) =
+                    if let Some(ref raw_value) = raw_value_opt {
+                        let current_ballot = u64::from_le_bytes(
+                            raw_value[..8].try_into().unwrap(),
+                        );
 
-                    let current_value = if raw_value[8..].is_empty() {
-                        None
+                        let current_value = if raw_value[8..].is_empty() {
+                            None
+                        } else {
+                            Some(raw_value[8..].to_vec())
+                        };
+                        (current_ballot, current_value)
                     } else {
-                        Some(raw_value[8..].to_vec())
+                        (0, None)
                     };
-                    (current_ballot, current_value)
+
+                if proposal.ballot > current_ballot {
+                    let mut serialized: Vec<u8> =
+                        proposal.ballot.to_le_bytes().to_vec();
+
+                    if let Some(value) = &proposal.value {
+                        serialized.extend_from_slice(value);
+                    }
+
+                    db.insert(&*key, serialized)?;
+                    //db.flush();
+
+                    Ok(Ok(()))
                 } else {
-                    (0, None)
-                };
-
-            if proposal.ballot > current_ballot {
-                let mut serialized: Vec<u8> =
-                    proposal.ballot.to_le_bytes().to_vec();
-
-                if let Some(value) = &proposal.value {
-                    serialized.extend_from_slice(value);
+                    Ok(Err(VersionedValue {
+                        ballot: current_ballot,
+                        value: current_value,
+                    }))
                 }
-
-                self.db.insert(&*key, serialized).expect("db io issue");
-
-                Ok(())
-            } else {
-                Err(VersionedValue {
-                    ballot: current_ballot,
-                    value: current_value,
-                })
-            }
-        };
-
-        // fsync our result before communicating anything to the client
-        self.db.flush().expect("db io issue");
-
-        ret
+            })
+            .expect("db io error")
     }
 }
